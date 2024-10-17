937fad1173429860a760ffe923223badfef83e07
TimurSatarov <timursatarov3@gmail.com>
2024-10-17 11:37:34 +0200

    test in different dir

--- build/main.c
+++ build/main.c
@@ -4,6 +4,11 @@
 int main(int argc, char const *argv[])
 {
     LinkedList *arrList = createLinkedList();
-    insert(arrList, 23);
+    insert(arrList, 0);
+    insert(arrList, 1);
+    insert(arrList, 2);
+    insert(arrList, 3);
+    add(arrList, 43);
+    print(arrList);
     return 0;
 }
